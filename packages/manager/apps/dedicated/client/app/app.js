a9304c539c6ba2672d5a191e6cad8b048cefeb19
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-03-23 17:21:18 +0530

    feat(dedicated): added cda module to dedicated

--- packages/manager/apps/dedicated/client/app/app.js
+++ packages/manager/apps/dedicated/client/app/app.js
@@ -38,6 +38,7 @@ import ovhPaymentMethod from '@ovh-ux/ng-ovh-payment-method';
 import uiRouter, { RejectType } from '@uirouter/angularjs';
 
 import moduleExchange from '@ovh-ux/manager-exchange';
+import ovhManagerCda from '@ovh-ux/manager-cda';
 import ovhManagerVeeamEnterprise from '@ovh-ux/manager-veeam-enterprise';
 import ovhManagerVeeamCloudConnect from '@ovh-ux/manager-veeam-cloud-connect';
 import ovhManagerVps from '@ovh-ux/manager-vps';
@@ -122,6 +123,7 @@ angular
       ovhManagerSupport,
       ovhManagerVeeamEnterprise,
       ovhManagerVeeamCloudConnect,
+      ovhManagerCda,
       ngTailLogs,
       ovhContacts,
       ovhManagerBanner,
